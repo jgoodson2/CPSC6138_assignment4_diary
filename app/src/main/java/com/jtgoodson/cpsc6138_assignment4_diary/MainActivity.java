c9f632e25f4fffb6c54adb5baf4ef5454b3a12ae
Jonathon Goodson <goodson.jonathon@gmail.com>
2018-07-15 19:00:36 -0400

    Add SignUp activity and logic in the main activity that will automatically redirect to the SignUp activity if a username has not been set in Shared Preferences"

--- app/src/main/java/com/jtgoodson/cpsc6138_assignment4_diary/MainActivity.java
+++ app/src/main/java/com/jtgoodson/cpsc6138_assignment4_diary/MainActivity.java
@@ -1,13 +1,39 @@
 package com.jtgoodson.cpsc6138_assignment4_diary;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "jonathon";
+    private static final String NVL_USERNAME = "no-username-set";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        SharedPreferences sharedPref = getSharedPreferences("userPass", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("username", null);
+        editor.apply();
+
+        String username = sharedPref.getString("username", NVL_USERNAME); //the second arg is like a nvl value
+        if (!username.equalsIgnoreCase(NVL_USERNAME)) {
+            Log.i(TAG, "username set; redirect to sign in: " + username);
+            setContentView(R.layout.activity_main);
+        } else {
+            Log.i(TAG, "username NOT set; redirect to sign up");
+            goToSignup();
+        }
+
+    }
+
+    private void goToSignup() {
+        Intent i = new Intent(this, SignUpActivity.class);
+        startActivity(i);
     }
 }
