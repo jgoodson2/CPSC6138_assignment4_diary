b710b24019d4c82c01748fae5aaf921b77d3054c
Jonathon Goodson <goodson.jonathon@gmail.com>
2018-07-15 19:56:59 -0400

    Rerig main activity to have sign up button; wire up logic on sign up screen

--- app/src/main/java/com/jtgoodson/cpsc6138_assignment4_diary/MainActivity.java
+++ app/src/main/java/com/jtgoodson/cpsc6138_assignment4_diary/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,25 +16,27 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        SharedPreferences sharedPref = getSharedPreferences("userPass", Context.MODE_PRIVATE);
+//        SharedPreferences.Editor editor = sharedPref.edit();
+//        editor.putString("username", null);
+//        editor.apply();
 
-        SharedPreferences sharedPref = getSharedPreferences("userPass", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putString("username", null);
-        editor.apply();
-
-        String username = sharedPref.getString("username", NVL_USERNAME); //the second arg is like a nvl value
-        if (!username.equalsIgnoreCase(NVL_USERNAME)) {
-            Log.i(TAG, "username set; redirect to sign in: " + username);
-            setContentView(R.layout.activity_main);
-        } else {
-            Log.i(TAG, "username NOT set; redirect to sign up");
-            goToSignup();
-        }
-
+//        String username = sharedPref.getString("username", NVL_USERNAME); //the second arg is like a nvl value
+//        if (!username.equalsIgnoreCase(NVL_USERNAME)) {
+//            Log.i(TAG, "username set; redirect to sign in: " + username);
+//            setContentView(R.layout.activity_main);
+//        } else {
+//            Log.i(TAG, "username NOT set; redirect to sign up");
+////            goToSignUp();
+//            Intent i = new Intent(this, SignUpActivity.class);
+////            startActivity(i);
+//        }
     }
 
-    private void goToSignup() {
+    public void goToSignUp(View view) {
         Intent i = new Intent(this, SignUpActivity.class);
         startActivity(i);
+        System.out.println("this is when i am supposed to go to sign up screen");
     }
 }
--- app/src/main/java/com/jtgoodson/cpsc6138_assignment4_diary/SignUpActivity.java
+++ app/src/main/java/com/jtgoodson/cpsc6138_assignment4_diary/SignUpActivity.java
@@ -1,13 +1,33 @@
 package com.jtgoodson.cpsc6138_assignment4_diary;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    public void saveInfo(View view) {
+
+        EditText username = (EditText) findViewById(R.id.txt_signup_username);
+        EditText password = (EditText) findViewById(R.id.txt_signup_password);
+
+        SharedPreferences sharedPref = getSharedPreferences("userPass", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("username", username.getText().toString());
+        editor.putString("password", password.getText().toString());
+        editor.apply();
+
+        Toast.makeText(this, "Saved!", Toast.LENGTH_LONG).show();
     }
 }
