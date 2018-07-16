1a2b14808f10387f3ac770e23c8e967723bf5883
jgoodson2 <jonathongoodson@clayton.edu>
2018-07-16 10:38:18 -0400

    Add helpful hint that will display user's authentication credentials since this isn't even a real app

--- app/src/main/java/com/jtgoodson/cpsc6138_assignment4_diary/MainActivity.java
+++ app/src/main/java/com/jtgoodson/cpsc6138_assignment4_diary/MainActivity.java
@@ -7,31 +7,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "jonathon";
     private static final String NVL_USERNAME = "no-username-set";
+    private static final String NVL_PASSWORD = "no-password-set";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        SharedPreferences sharedPref = getSharedPreferences("userPass", Context.MODE_PRIVATE);
-//        SharedPreferences.Editor editor = sharedPref.edit();
-//        editor.putString("username", null);
-//        editor.apply();
 
-//        String username = sharedPref.getString("username", NVL_USERNAME); //the second arg is like a nvl value
-//        if (!username.equalsIgnoreCase(NVL_USERNAME)) {
-//            Log.i(TAG, "username set; redirect to sign in: " + username);
-//            setContentView(R.layout.activity_main);
-//        } else {
-//            Log.i(TAG, "username NOT set; redirect to sign up");
-////            goToSignUp();
-//            Intent i = new Intent(this, SignUpActivity.class);
-////            startActivity(i);
-//        }
+        SharedPreferences sharedPref = getSharedPreferences("userPass", Context.MODE_PRIVATE);
+
+        String username = sharedPref.getString("username", NVL_USERNAME);
+        String password = sharedPref.getString("password", NVL_PASSWORD);
+        TextView userPwHint = (TextView) findViewById(R.id.lbl_userPwHint);
+        if (!username.equalsIgnoreCase(NVL_USERNAME))
+            userPwHint.setText("(Hint: user credentials are " + username + "/" + password + " )");
     }
 
     public void goToSignUp(View view) {
