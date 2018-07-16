9aa9040f30db36e977d8b4555821115bedc054f2
jgoodson2 <jonathongoodson@clayton.edu>
2018-07-16 15:32:48 -0400

    Add authentication into main diary screen

--- app/src/main/java/com/jtgoodson/cpsc6138_assignment4_diary/MainActivity.java
+++ app/src/main/java/com/jtgoodson/cpsc6138_assignment4_diary/MainActivity.java
@@ -5,9 +5,10 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +28,26 @@ public class MainActivity extends AppCompatActivity {
         TextView userPwHint = (TextView) findViewById(R.id.lbl_userPwHint);
         if (!username.equalsIgnoreCase(NVL_USERNAME))
             userPwHint.setText("(Hint: user credentials are " + username + "/" + password + " )");
+    }
+
+    public void authenticateUser(View view) {
+        SharedPreferences sharedPref = getSharedPreferences("userPass", Context.MODE_PRIVATE);
+        String username = sharedPref.getString("username", NVL_USERNAME);
+        String password = sharedPref.getString("password", NVL_PASSWORD);
+
+        EditText input_username = (EditText) findViewById(R.id.txt_signin_username);
+        EditText input_password = (EditText) findViewById(R.id.txt_signin_password);
+
+        if (!(input_username.getText().toString().equalsIgnoreCase(username) && input_password.getText().toString().equals(password))) {
+            System.out.println("authentication failed");
+            Toast.makeText(this, R.string.invalid_credentials, Toast.LENGTH_LONG).show();
+
+        } else {
+            System.out.println("user authenticated successfully");
+            Intent i = new Intent(this, DiaryMain.class);
+            startActivity(i);
+        }
+
     }
 
     public void goToSignUp(View view) {
