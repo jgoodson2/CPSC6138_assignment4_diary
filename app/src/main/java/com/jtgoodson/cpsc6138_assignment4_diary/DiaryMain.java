4de77e8e41ed468dec48a4eb03849846fabae948
jgoodson2 <jonathongoodson@clayton.edu>
2018-07-16 16:38:56 -0400

    Add controls for add new diary entry screen

--- app/src/main/java/com/jtgoodson/cpsc6138_assignment4_diary/DiaryMain.java
+++ app/src/main/java/com/jtgoodson/cpsc6138_assignment4_diary/DiaryMain.java
@@ -1,7 +1,9 @@
 package com.jtgoodson.cpsc6138_assignment4_diary;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class DiaryMain extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class DiaryMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary_main);
+    }
+
+    public void goToNewDiaryEntry(View view) {
+        Intent i = new Intent(this, DiaryNewEntryActivity.class);
+        startActivity(i);
     }
 }
