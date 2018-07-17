d4ae3123c38872b7fe94a8a19cb694a0bd64ed90
Jonathon Goodson <goodson.jonathon@gmail.com>
2018-07-16 20:12:39 -0400

    Add contructors, getters, setters to class DiaryEntry

--- app/src/main/java/com/jtgoodson/cpsc6138_assignment4_diary/DiaryEntry.java
+++ app/src/main/java/com/jtgoodson/cpsc6138_assignment4_diary/DiaryEntry.java
@@ -2,6 +2,36 @@ package com.jtgoodson.cpsc6138_assignment4_diary;
 
 public class DiaryEntry {
 
-    //private int
+    private int _id;
+    private String _subject;
+    private String _content;
 
+    public DiaryEntry(String subject, String content) {
+        this._subject = subject;
+        this._content = content;
+    }
+
+    public int get_id() {
+        return _id;
+    }
+
+    public void set_id(int _id) {
+        this._id = _id;
+    }
+
+    public String get_subject() {
+        return _subject;
+    }
+
+    public void set_subject(String _subject) {
+        this._subject = _subject;
+    }
+
+    public String get_content() {
+        return _content;
+    }
+
+    public void set_content(String _content) {
+        this._content = _content;
+    }
 }
